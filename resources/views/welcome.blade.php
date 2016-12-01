936fd3f041d592314fd7354c70a935e5e612087c
lonac <barakamalonao@yahoo.com>
2016-12-01 22:12:54 +0300

    edited header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,5 +68,22 @@
                             </p>
                       </div>
             </div>
+
+    <h2 class="alt-h2">Mpung@ grows you richer!</h2>
+        <div class="clearfix gut-lg">
+          <div class="float-md-left col-md-4 my-4">
+            <h3 class="alt-h3 my-2"><a href="/personal" class="text-blue octicon-middle">Individuals <svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
+            <p class="text-gray">You can Register your own Financial institution, if you have enough money or you wnat to start organazation and have ability to have as much customer as you can.</p>
+          </div>
+          <div class="float-md-left col-md-4 my-4">
+            <h3 class="alt-h3 my-2"><a href="/open-source" class="text-orange octicon-middle">Organization<svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
+            <p class="text-gray">Mpung@ helps you as a manager to register your organization and set policies and rule to allow different people to have loans from you!.</p>
+          </div>
+          <div class="float-md-left col-md-4 my-4">
+            <h3 class="alt-h3 my-2"><a href="/business" class="text-purple octicon-middle">Financial services <svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
+            <p class="text-gray">The site allows for registered organizations to host their products and sell them online..</p>
+          </div>
+      </div>
     </div>
+
 @endsection
