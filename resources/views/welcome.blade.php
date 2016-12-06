9f9b3789f0e061919dcfc12f086a9ac0c903239d
lonac <barakamalonao@yahoo.com>
2016-12-06 19:46:12 +0300

    changes on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,23 @@
 @section('content')
     <div class="container">
         <div class="row banner">
-
-            <h1 class="text-center margin-top-10 editContent">
-                    Zaidi ya kampuni 1,000 kukupa mpunga kwa riba ndogo.....!
-                </h1>
-
-            <div class="col-md-13" style="background-color: #000000; box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;"> 
-                <small>Grow richer...!!</small>
-                <div class="text-center">
-                    <img src="https://s-media-cache-ak0.pinimg.com/736x/5e/26/bd/5e26bde4c8803fecec9ace5d685e73f8.jpg" width="600" height="290" alt="" class="rounded">
+            <div class="col-sm-8 col-md-12">
+            <div class="side-borders">
+                    <div class="hero-bg" style="background-image:url('/images/mpunga.png');background-repeat: no-repeat;background-size: 100% 100%; height: 350px;">
+                        <div class="hero-cnt medium-8 large-6" align="right">
+                            <p><h1>Zaidi ya kampuni 10000 kukupa</p></h1>
+                                  <h1><p>pesa kwa asilimia ndogo.....!</p></h1>
+                            
+                            <div class="cta">
+                                    <a href="/compare-accounts.html" class="button alt">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <small>Kujisajili ni bure....!</small>
             </div>
-
         </div>
 
         <div class="row">
-
             <div class="col-sm-6 col-md-3">
                 <div class="caption">
                     <h3>5BFs Company</h3>
@@ -72,15 +72,15 @@
     <h2 class="alt-h2">Mpung@ grows you richer!</h2>
         <div class="clearfix gut-lg">
           <div class="float-md-left col-md-4 my-4">
-            <h3 class="alt-h3 my-2"><a href="/personal" class="text-blue octicon-middle">Individuals <svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
+            <h3 class="alt-h3 my-2"><a href="#" class="text-blue octicon-middle">Individuals <svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
             <p class="text-gray">You can Register your own Financial institution, if you have enough money or you wnat to start organazation and have ability to have as much customer as you can.</p>
           </div>
           <div class="float-md-left col-md-4 my-4">
-            <h3 class="alt-h3 my-2"><a href="/open-source" class="text-orange octicon-middle">Organization<svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
+            <h3 class="alt-h3 my-2"><a href="#" class="text-orange octicon-middle">Organization<svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
             <p class="text-gray">Mpung@ helps you as a manager to register your organization and set policies and rule to allow different people to have loans from you!.</p>
           </div>
           <div class="float-md-left col-md-4 my-4">
-            <h3 class="alt-h3 my-2"><a href="/business" class="text-purple octicon-middle">Financial services <svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
+            <h3 class="alt-h3 my-2"><a href="#" class="text-purple octicon-middle">Financial services <svg aria-hidden="true" class="octicon octicon-chevron-right" height="22" version="1.1" viewBox="0 0 8 16" width="11"><path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3z"/></svg></a></h3>
             <p class="text-gray">The site allows for registered organizations to host their products and sell them online..</p>
           </div>
       </div>
